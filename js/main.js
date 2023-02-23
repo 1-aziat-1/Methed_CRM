13a18c67662898bcb4d9da53807fa61b1fa48cc0
1-aziat-1 <as.ac2727@mail.ru>
2023-02-23 16:43:23 +0300

    dev commit

---
+++ js/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const modal = document.querySelector('.modal');
+const modalTitle = modal.querySelector('.modal__title');
+const modalCloseBtn = modal.querySelector('.modal__btn-close');
+const modalForm = modal.querySelector('.modal__form');
+const modalCheckbox = modal.querySelector('.form__item-checkbox');
+const modalInputCheckbox = modal.querySelector('.form__item-input-discount');
+const modalResultCosts = modal.querySelector('.modal__footer-costs>span');
+const modalId = modal.querySelector('.modal__id');
