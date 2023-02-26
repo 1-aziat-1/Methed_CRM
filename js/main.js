6aa4b07e14ae562ed7c26293c2c19d3e50e014e5
1-aziat-1 <as.ac2727@mail.ru>
2023-02-26 12:49:36 +0300

    fixed add tr and td in table

--- js/main.js
+++ js/main.js
@@ -70,19 +70,73 @@ const modalId = modal.querySelector('.modal__id');
 
 const tbody = document.querySelector('.cms__body');
 
-const createRow = (obj) => {
+const createBtn = () => {
+  const tdButton = document.createElement('td');
+  tdButton.classList.add('body-item', 'body-icon');
+
+  const btnImg = document.createElement('button');
+  btnImg.classList.add('body-icon__btn-img');
+
+  const btnFix = document.createElement('button');
+  btnFix.classList.add('body-icon__btn-fix');
+
+  const btnDelete = document.createElement('button');
+  btnDelete.classList.add('body-icon__btn-delete');
+
+  tdButton.append(btnImg, btnFix, btnDelete);
+
+  return {
+    tdButton,
+    btnImg,
+    btnFix,
+    btnDelete,
+  };
+};
+
+const createRow = ({id, title, category, units, count, price}) => {
   const tr = document.createElement('tr');
-  for (let i in obj) {
-    const td = document.createElement('td');
-    td.textContent = obj[i];
-    tr.append(td);
-  }
+  tr.classList.add('cms__body-list');
+
+  const tdId = document.createElement('td');
+  tdId.classList.add('body-item', 'body-id');
+  tdId.textContent = id;
+
+  const tdTitle = document.createElement('td');
+  tdTitle.classList.add('body-item', 'body-name');
+  tdTitle.textContent = title;
+
+  const tdCategory = document.createElement('td');
+  tdCategory.classList.add('body-item', 'body-category');
+  tdCategory.textContent = category;
+
+  const tdUnits = document.createElement('td');
+  tdUnits.classList.add('body-item', 'body-unit');
+  tdUnits.textContent = units;
+
+  const tdCount = document.createElement('td');
+  tdCount.classList.add('body-item', 'body-count');
+  tdCount.textContent = count;
+
+  const tdPrice = document.createElement('td');
+  tdPrice.classList.add('body-item', 'body-cost');
+  tdPrice.textContent = price;
+
+  const tdResult = document.createElement('td');
+  tdResult.classList.add('body-item', 'body-result');
+  tdResult.textContent = price*count;
+
+  const btn = createBtn();
+  const {tdButton} = btn;
+
+
+  tr.append(tdId, tdTitle, tdCategory, tdUnits, tdCount, tdPrice, tdResult, tdButton);
+  
   return tr;
 };
 
 const renderGoods = (arrObj) => {
   const massiv = arrObj.map(item => createRow(item));
-  return massiv;
+  tbody.append(...massiv);
 };
 
 
